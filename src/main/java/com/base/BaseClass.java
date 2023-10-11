8f309035e800bb4194c532eb87fd854b092d495f
Saylee <sayleesodegaonkar@gmail.com>
2023-10-11 16:43:25 +0530

    Dependancy Added

---
+++ src/main/java/com/base/BaseClass.java
@@ -0,0 +1,15 @@
+package com.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseClass {
+
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		
+	}
+
+}
